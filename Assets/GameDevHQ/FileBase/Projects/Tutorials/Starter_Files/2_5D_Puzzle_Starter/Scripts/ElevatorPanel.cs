874d43c308954d29c15b96c47b13b7e551c94c84
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-01-06 18:03:26 -0500

    Elevator Light

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/ElevatorPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _elevatorLight;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Called");
+                _elevatorLight.material.color = Color.green;
+            }
+        }
+    }
+}
